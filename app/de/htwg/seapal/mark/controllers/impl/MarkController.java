733989310b6c5d9eba64fae36ce9c58436eccace
dafoelle <david@foellenz.de>
2013-04-15 11:43:20 +0200

    Persistence added as interface and impl

--- app/de/htwg/seapal/mark/controllers/impl/MarkController.java
+++ app/de/htwg/seapal/mark/controllers/impl/MarkController.java
@@ -1,16 +1,20 @@
 package de.htwg.seapal.mark.controllers.impl;
 
+import com.db4o.ObjectContainer;
 import com.google.inject.Inject;
 import com.google.inject.Singleton;
 
 import de.htwg.seapal.mark.controllers.AbstractMarkController;
-import de.htwg.seapal.mark.models.IMark;
+import de.htwg.seapal.mark.models.impl.Mark;
 
 @Singleton 
 public class MarkController extends AbstractMarkController{
 	
+	protected ObjectContainer db = null;
+	protected String filename = "marks.db4o";
+	
 	@Inject
-	public MarkController(IMark mark) {
+	public MarkController(Mark mark) {
 		this.mark = mark;
 	}
 }
